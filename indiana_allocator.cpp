280795ef6137736138d2b9cdea0a577dc63ccb49
krishna.vaidy <krishnarules@gmail.com>
2016-03-29 19:49:30 -0400

    Indiana allocation

--- indiana_allocator.cpp
+++ indiana_allocator.cpp
@@ -12,11 +12,11 @@ using namespace std;
 static const int pageSize = 4;
 
 // Ram size in GB
-static const int ramSize = 4;
+static const int ramSize = 16;
 
 // Number of frames
-//static const unsigned long nPages = long((ramSize * pow(2, 30))/(4 * pow(2,10)));
-static const unsigned long nPages = 100;
+static const unsigned long nPages = long((ramSize * pow(2, 30))/(4 * pow(2,10)));
+//static const unsigned long nPages = 
 
 // Size after which to move nodes from inactive queue to free queue
 static const unsigned long moveSize = long(nPages* 0.05);
@@ -31,9 +31,16 @@ static const int frameSize = sizeof(struct page_t);
 
 // Memseg datastructure
 struct memseg {
-    page_t * pages, * epages;
+    page_t * pages;
+    page_t * epages;
     struct memseg * next;
     struct memseg * lnext;
+    memseg(){
+        pages = NULL;
+        epages = NULL;
+        next = NULL;
+        lnext = NULL;
+    }
 };
 
 //memsegSize
@@ -45,13 +52,14 @@ struct node {
     struct node* next;
     node() {
         next = NULL;
+        page_address = NULL;
     }
 };
 
 // Declare classes
+class IndianaStructure;
 class Queue;
 class MemsegQueue;
-class IndianaStructure;
 
 // Class which implements Queue
 // TODO: Refactor to implement templates
@@ -228,15 +236,45 @@ double avg(std::vector<double> d){
         return sum/(float)d.size();
     return 0;
 }
+class IndianaStructure{
+    private:
+        Queue activeQ;
+        MemsegQueue inactiveQ, freeQ;
 
-void IndianaStructure::addActiveQ(const int n){
+    public:
+        int sizeActiveQ(){
+            return activeQ.size;
+        }
+
+        int sizeInactiveQ(){
+            return inactiveQ.size;
+        }
+
+        int sizeFreeQ(){
+            return freeQ.size;
+        }
+
+        void addActiveQ(const int n);
+        void removeActiveQ(const int n);
+
+        void initFreeQ(page_t *page, page_t *epage) {
+            freeQ.enqueue(page, epage);
+        }
+        void addFreeQ(page_t * page);
+        std::vector<page_t *> removeFreeQ(const int n);
+
+        void addInactiveQ(page_t * page);
+        std::vector<page_t *> removeInactiveQ(const int n);
+};
+
+void IndianaStructure::addActiveQ(const int n) {
     // Check if freeQ has enough meory to allocate n pages to activeQ
     if(n > freeQ.size){
         cout<<"\nNot enough memory in FreeQ"<<"\n n: "<<n<<"\n free queue size: "<<freeQ.size;
         exit(EXIT_FAILURE);
     }
     // Get n pages from freeQ
-    std::vector<page_t *> freePages = removeFreeQ(freeQ, n);
+    std::vector<page_t *> freePages = removeFreeQ(n);
     //cout<<"\nRemoved from freeQ successfully";
 
     // Add to activeQ
@@ -261,10 +299,10 @@ void IndianaStructure::addActiveQ(const int n){
 
 void IndianaStructure::removeActiveQ(const int n){
     for(int i=0; i<n; i++){
-        if(activeQ.isempty()){
+        if(this->activeQ.isempty()){
             break;
         }
-        addFreeQ(freeQ, activeQ.dequeue());
+        this->addFreeQ(this->activeQ.dequeue());
     }
 }
 
@@ -273,13 +311,16 @@ void IndianaStructure::addInactiveQ(page_t * page){
     do{
         if( (tmp->pages - frameSize) == page){
             tmp->pages = page;
+            this->inactiveQ.size++;
             return;
         }
         else if ( (tmp->epages + frameSize) == page){
             tmp->epages = page;
+            this->inactiveQ.size++;
             return;
         }
-    }while(tmp!=inactiveQ.tail);
+        tmp = tmp->next;
+    }while(tmp!=this->inactiveQ.tail && tmp !=NULL);
 
     // When page is not contiguous to any existing blocks 
     tmp = new memseg();
@@ -287,20 +328,24 @@ void IndianaStructure::addInactiveQ(page_t * page){
     tmp->epages = page;
     inactiveQ.tail->next = tmp;
     inactiveQ.tail = tmp;
+    this->inactiveQ.size++;
 }
 
 void IndianaStructure::addFreeQ(page_t * page){
-    struct memseg * tmp = freeQ.head;
+    struct memseg * tmp = this->freeQ.head;
     do{
-        if( (tmp->pages - frameSize) == page){
+        if((tmp->pages - frameSize) == page){
             tmp->pages = page;
+            this->freeQ.size++;
             return;
         }
-        else if ( (tmp->epages + frameSize) == page){
+        else if((tmp->epages + frameSize) == page){
             tmp->epages = page;
+            this->freeQ.size++;
             return;
         }
-    }while(tmp!=freeQ.tail);
+        tmp = tmp->next;
+    }while(tmp!=this->freeQ.tail && tmp !=NULL);
 
     // When page is not contiguous to any existing blocks 
     tmp = new memseg();
@@ -308,6 +353,7 @@ void IndianaStructure::addFreeQ(page_t * page){
     tmp->epages = page;
     freeQ.tail->next = tmp;
     freeQ.tail = tmp;
+    this->freeQ.size++;
 }
 
 int min(const int x, const int y){
@@ -367,37 +413,6 @@ std::vector<page_t *> IndianaStructure::removeInactiveQ(int n){
     return removedPages;
 }
 
-class IndianaStructure{
-    private:
-        Queue activeQ;
-        MemsegQueue inactiveQ, freeQ;
-
-    public:
-        int sizeActiveQ(){
-            return activeQ.size;
-        }
-
-        int sizeInactiveQ(){
-            return inactiveQ.size;
-        }
-
-        int sizeFreeQ(){
-            return freeQ.size;
-        }
-
-        void addActiveQ(const int n);
-        void removeActiveQ(const int n);
-
-        void initFreeQ(page_t *page, page_t *epage) {
-            freeQ.enqueue(page, epage);
-        }
-        void addFreeQ(page_t * page);
-        std::vector<page_t *> removeFreeQ(const int n);
-
-        void addInactiveQ(page_t * page);
-        std::vector<page_t *> removeInactiveQ(const int n);
-};
-
 int main() {
 
     std::vector<double> initTime, free1Time, free2Time, allocate1Time, allocate2Time;
@@ -417,67 +432,64 @@ int main() {
         I.initFreeQ(&memory[0], &memory[nPages-1]);
         initStop = microsec_clock::universal_time();
 
-        cout<<"\nFreeQ size: "<<freeQ.size;
+        cout<<"\nFreeQ size: "<<I.sizeFreeQ();
 
         // Set random operations
         std::random_device rd; // obtain a random number from hardware
         std::mt19937 eng(rd()); // seed the generator
-        std::uniform_int_distribution<> distr(2, 10); // define the range
+        std::uniform_int_distribution<> distr(2, 1000); // define the range
 
         allocate1Start = microsec_clock::universal_time();
         // Allocate 3/4 memory in random size amounts
         while(I.sizeActiveQ() < 0.75*nPages){
             const int step = int(distr(eng));
             I.addActiveQ(step);
-            cout<<endl<<step<<"\nactive size: "<<I.sizeActiveQ();
+            //cout<<endl<<step<<"\nactive size: "<<I.sizeActiveQ();
         }
         allocate1Stop = microsec_clock::universal_time();
-        cout<<"\nAllocated 3/4 of memory";
+        //cout<<"\nAllocated 3/4 of memory";
 
-        //// Free down to 1/2 of memory
-        //free1Start = microsec_clock::universal_time();
-        //removeActiveQ(activeQ, freeQ, nPages/4);
-        //free1Stop = microsec_clock::universal_time();
+        // Free down to 1/2 of memory
+        free1Start = microsec_clock::universal_time();
+        I.removeActiveQ(nPages/4);
+        free1Stop = microsec_clock::universal_time();
         //cout<<"\nFreed down to 1/2 of memory";
 
-        //allocate2Start = microsec_clock::universal_time();
-        //// Randomly allocate upto 3/4 memory from 1/2
-        //while(activeQ.size < 0.75*nPages){
-            //const int step = int(distr(eng));
-            //addActiveQ(activeQ, freeQ, inactiveQ, step);
-        //}
-        //allocate2Stop = microsec_clock::universal_time();
+        allocate2Start = microsec_clock::universal_time();
+        // Randomly allocate upto 3/4 memory from 1/2
+        while(I.sizeActiveQ() < 0.75*nPages){
+            const int step = int(distr(eng));
+            I.addActiveQ(step);
+        }
+        allocate2Stop = microsec_clock::universal_time();
         //cout<<"\nAllocate again upto 3/4 of memory";
 
-        //free2Start = microsec_clock::universal_time();
-        //// Free all memory
-        //removeActiveQ(activeQ, freeQ, 0.75*nPages);
-        //free2Stop = microsec_clock::universal_time();
+        free2Start = microsec_clock::universal_time();
+        // Free all memory
+        I.removeActiveQ(0.75*nPages);
+        free2Stop = microsec_clock::universal_time();
         //cout<<"\nFreed all memory";
 
-        //boost::posix_time::time_duration initTimeD = -(initStart-initStop);
-        //boost::posix_time::time_duration allocate1TimeD = -(allocate1Start - allocate1Stop);
-        //boost::posix_time::time_duration allocate2TimeD = -(allocate2Start - allocate2Stop);
-        //boost::posix_time::time_duration free1TimeD = -(free1Start - free1Stop);
-        //boost::posix_time::time_duration free2TimeD = -(free2Start - free2Stop);
+        boost::posix_time::time_duration initTimeD = -(initStart-initStop);
+        boost::posix_time::time_duration allocate1TimeD = -(allocate1Start - allocate1Stop);
+        boost::posix_time::time_duration allocate2TimeD = -(allocate2Start - allocate2Stop);
+        boost::posix_time::time_duration free1TimeD = -(free1Start - free1Stop);
+        boost::posix_time::time_duration free2TimeD = -(free2Start - free2Stop);
         
-        //initTime.push_back(initTimeD.total_nanoseconds());
-        //allocate1Time.push_back(allocate1TimeD.total_nanoseconds());
-        //allocate2Time.push_back(allocate2TimeD.total_nanoseconds());
-        //free1Time.push_back(free1TimeD.total_nanoseconds());
-        //free2Time.push_back(free2TimeD.total_nanoseconds());
+        initTime.push_back(initTimeD.total_nanoseconds());
+        allocate1Time.push_back(allocate1TimeD.total_nanoseconds());
+        allocate2Time.push_back(allocate2TimeD.total_nanoseconds());
+        free1Time.push_back(free1TimeD.total_nanoseconds());
+        free2Time.push_back(free2TimeD.total_nanoseconds());
  
     }
-/*
- *    std::cout<<"\nTime Taken";
- *    std::cout<<"\n----------";
- *    std::cout<<"\nInit time: "<<avg(initTime);
- *    std::cout<<"\nAllocating 3/4th memory: "<<avg(allocate1Time);
- *    std::cout<<"\nAllocating from 1/2 memory to 3/4th memory: "<<avg(allocate2Time);
- *    std::cout<<"\nFreeing memory from 3/4 memory to 1/2: "<<avg(free1Time);
- *    std::cout<<"\nFreeing memory from 3/4 memory to empty: "<<avg(free2Time)<<std::endl;
- *
- */
+    std::cout<<"\nTime Taken";
+    std::cout<<"\n----------";
+    std::cout<<"\nInit time: "<<avg(initTime);
+    std::cout<<"\nAllocating 3/4th memory: "<<avg(allocate1Time);
+    std::cout<<"\nAllocating from 1/2 memory to 3/4th memory: "<<avg(allocate2Time);
+    std::cout<<"\nFreeing memory from 3/4 memory to 1/2: "<<avg(free1Time);
+    std::cout<<"\nFreeing memory from 3/4 memory to empty: "<<avg(free2Time)<<std::endl;
 }
 
 
